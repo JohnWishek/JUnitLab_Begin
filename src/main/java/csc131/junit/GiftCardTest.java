a2e6305c768f4e407f67b5cba3fc4bbb1b2bc4da
John <johnwishek@yahoo.com>
2021-03-14 16:39:30 -0700

    -added 3 more tests for constuctor throw and added fuller coverage for previous deduct test

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -1,7 +1,7 @@
 package csc131.junit;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class GiftCardTest {
 
@@ -38,12 +38,13 @@ public class GiftCardTest {
 		int issuingStore;
 		double amount;
 		
-		amount = 100;
+		
 		issuingStore = 1337;
 		balance = 100.00;
 		card = new GiftCard(issuingStore, balance);
 		String      result;
-	        
+	    for (amount = -50; amount < 100; amount++) 
+	    {    
 	        if (amount < 0.0)
 	        {
 	            result = "Invalid Transaction";
@@ -65,5 +66,21 @@ public class GiftCardTest {
 	            }
 	        }
 		assertEquals("deduct()", result, card.deduct(amount));
+	    }
+	}
+	@Test
+	public void badConstructorBalance()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	@Test
+	public void constructor_IncorrectID_Low()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.00);});
+	}
+	@Test
+	public void constructor_IncorrectID_High() 
+	{
+        assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000,100.00);});
 	}
 }
